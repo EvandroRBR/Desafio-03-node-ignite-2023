22455e76598a8a4c16f4cda5f3ecc52bf951b802
EvandroRBR <evandro.braga@beedoo.com.br>
2023-05-20 17:56:22 -0300

    feat: PrismaPetsRepository

---
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -0,0 +1,17 @@
+import { Prisma } from '@prisma/client';
+import { IPetsRepository } from '../pets-repository';
+import { prisma } from '@/lib/prisma';
+
+export class PrismaPetsRepository implements IPetsRepository {
+  async findManyByCity(city: string) {
+    const petsByCity = await prisma.pet.findMany({ where: { city } });
+
+    return petsByCity;
+  }
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({ data });
+
+    return pet;
+  }
+}
