eb3f9cf3313162922140401043ddfefdb575d715
EvandroRBR <evandro.braga@beedoo.com.br>
2023-05-21 08:23:47 -0300

    test: Teste e2e para regitro de pet

---
+++ src/http/controllers/pets/register-pet-controller.spec.ts
@@ -0,0 +1,34 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { randomUUID } from 'crypto';
+
+import { app } from '@/app';
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
+
+describe('Register Pet e2e', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to register a pet', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+    const registerPetResponse = await request(app.server)
+      .post('/pets')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Bud',
+        city: 'São José dos Campos',
+        description: 'Animal legal',
+        age: '4',
+        orgId: randomUUID(),
+      });
+
+    expect(registerPetResponse.statusCode).toEqual(201);
+    expect(registerPetResponse.body).toEqual(
+      expect.objectContaining({ name: 'Bud' }),
+    );
+  });
+});
